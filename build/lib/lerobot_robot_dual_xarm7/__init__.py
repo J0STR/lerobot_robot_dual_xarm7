1edb3d551121d80e8712bce02b72a820ffccf391
Jonas Stehr <jonas.stehr@hs-ruhrwest.de>
2026-02-12 14:58:48 +0100

    first commit

---
+++ build/lib/lerobot_robot_dual_xarm7/__init__.py
@@ -0,0 +1,2 @@
+from .config_dual_xarm7 import Dual_xArm7Config
+from .dual_xarm7 import Dual_xArm7
---
+++ build/lib/lerobot_robot_dual_xarm7/config_dual_xarm7.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass, field
+
+from lerobot.cameras import CameraConfig
+from lerobot.cameras.opencv import OpenCVCameraConfig
+from lerobot.cameras.realsense import RealSenseCameraConfig
+from lerobot.cameras import ColorMode, Cv2Rotation
+from lerobot.robots import RobotConfig
+
+
+@RobotConfig.register_subclass("Dual_xArm7")
+@dataclass
+class Dual_xArm7Config(RobotConfig):
+    id: str
+    robot_ip_right: str = "10.2.134.152"
+    robot_ip_left: str = "10.2.134.151"
+
+
+    cameras: dict[str, CameraConfig] = field(
+        default_factory=lambda: {
+            # "top_down": OpenCVCameraConfig(
+            #     index_or_path="/dev/video4",
+            #     fps=30,
+            #     width=640,
+            #     height=480,
+            # ),
+            "wrist_right": RealSenseCameraConfig(
+                serial_number_or_name="352122273091",
+                fps=30,
+                width=640,
+                height=480),
+            "wrist_left": RealSenseCameraConfig(
+                serial_number_or_name="409122271056",
+                fps=30,
+                width=640,
+                height=480),
+        }
+    )
